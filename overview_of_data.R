fd32c896d1a496bb6cdd662b704ecde380340ead
Georgy Kalashnov <kalashnov@MacBook-Air-Georgy.local>
2021-02-10 17:10:25 +0300

    overview of schools

--- overview_of_data.R
+++ overview_of_data.R
@@ -75,3 +75,27 @@ df %>%
 # smth bad
 
 ######## IMMUNIZATION IMITATION DOESN'T HELP ############
+
+
+############# IMUNIZATION #############
+
+set.seed(0)
+school_data <- prepare_school_data()
+df <- build_models(school_data)
+
+
+df %>%
+  cumulative_te(cpredictions_1) %>%
+  cumulative_te(random) %>%
+  cumulative_te(predictions) %>%
+  cumulative_te(cpredictions) %>%
+  cumulative_te(cpredictions_regr) %>%
+  ggplot() + geom_line(aes(y=cumulative_Y1_random, x=cumulative_Y2_random, color='random')) +
+  geom_line(aes(y=cumulative_Y1_predictions, x=cumulative_Y2_predictions, color='IV')) +
+  geom_line(mapping=aes(y=cumulative_Y1_cpredictions_1, x=cumulative_Y2_cpredictions_1, color='model O only')) +
+  geom_line(mapping=aes(y=cumulative_Y1_cpredictions, x=cumulative_Y2_cpredictions, color='knapsack')) +
+  geom_line(mapping=aes(y=cumulative_Y1_cpredictions_regr, x=cumulative_Y2_cpredictions_regr, color='diff')) +
+  labs(x='Number of vouchers realized per h/h', y='Number of malaria treatments bought for children under 14') #+
+
+
+# smth bad
